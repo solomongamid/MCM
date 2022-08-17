5371ded574d481f36c4e7793a4c107791ed226d3
suliman_Abdel <suliman.abdelhadeir-ext@opcoep.fr>
2022-08-17 12:44:36 +0200

    Get opportunity currency and then display its rate from Active Currencies rate table

--- MCM/force-app/main/default/lwc/currenciesRateDisplayer/currenciesRateDisplayer.js
+++ MCM/force-app/main/default/lwc/currenciesRateDisplayer/currenciesRateDisplayer.js
@@ -1,28 +1,87 @@
-import { LightningElement, track, wire } from 'lwc';
-import getAllCurrencies from '@salesforce/apex/DisplayCurrenciesRate_CTR.getAllCurrencies';
+import { LightningElement, track, wire, api } from "lwc";
+import getAllCurrencies from "@salesforce/apex/DisplayCurrenciesRate_CTR.getAllCurrencies";
+import { getRecord, getFieldValue } from "lightning/uiRecordApi";
 
-const columns = [{
-    label: 'Currency ISO Code',
-    fieldName: 'IsoCode',
-    type: 'text',
+import CurrencyIsoCode_FIELD from "@salesforce/schema/Opportunity.CurrencyIsoCode";
+
+const columns = [
+  {
+    label: "Currency ISO Code",
+    fieldName: "IsoCode",
+    type: "text",
     sortable: true
-},
-{
-    label: 'Conversion Rate',
-    fieldName: 'ConversionRate',
-    type: 'Currency',
+  },
+  {
+    label: "Conversion Rate",
+    fieldName: "ConversionRate",
+    type: "currency",
+    typeAttributes: { step: "0.01" }, //add currencyCode: 'EUR' to to display currency on Euro
+    cellAttributes: { alignment: "left" },
     sortable: true
-}
+  }
 ];
-export default class CurrenciesRateDisplayer extends LightningElement {
-    columns = columns;
-    @track results;
-    defaultSortDirection = 'asc';
+const fields = [CurrencyIsoCode_FIELD];
 
-    @wire(getAllCurrencies, {}) 
-    valueList({ error, data }) {
-        console.log(data);
-        this.results = data;
-        this.error = error;
+export default class CurrenciesRateDisplayer extends LightningElement {
+  @api recordId;
+  columns = columns;
+  @track currCodes;
+  @track error;
+  @track oppCurrIsoCodeValue;
+  @track corporateCurr = "EURO";
+  @track connectedCallbackexecuted = false;
+  @track oppCurrCoversionRate;
+  defaultSortDirection = "asc";
+  oppRecord;
+  oppError;
+
+  //TODO : Dispaly only opportunity currency Exchange rate
+  @wire(getRecord, { recordId: "$recordId", fields })
+  wiredOpp({data, error}){
+    if(data){
+      console.log("this.data INfo :: " + JSON.stringify(data));
+      this.oppRecord = data;
+      this.oppCurrIsoCodeValue = this.oppRecord.fields.CurrencyIsoCode.value;
+      console.log("this.this.oppCurrIsoCodeValue Wired INfo :: " + this.oppCurrIsoCodeValue );
+    } else if(error){
+      
+      this.oppError = error;
     }
+  }
+
+  //TODO : Use configured variables to let admin choose btn display all change rates or only opportunity change rate
+
+  connectedCallback() {
+    var corporateCurrItem;
+    this.connectedCallbackexecuted = false;
+    console.log("this.recordId INfo :: " + this.recordId);
+    console.log("this.oppCurrIsoCodeValue INfo :: " + this.oppCurrIsoCodeValue);
+    getAllCurrencies()
+      .then((results) => {
+        this.currCodes = results;
+        this.error = undefined;
+        console.log("this.currCodes INfo :: " + JSON.stringify(this.currCodes));
+        console.log("this.corporateCurr INfo :: " + this.corporateCurr);
+        corporateCurrItem = this.currCodes.find((obj) => {
+          return obj.IsCorporate === true;
+        });
+        this.corporateCurr = corporateCurrItem.IsoCode;
+        console.log("this.corporateCurr INfo :: " + this.corporateCurr);
+        //As renderedCallback executes twice before and after getAllCurrencies, so put a variable to execute only after getAllCurrencies method
+        this.connectedCallbackexecuted = true;
+      })
+      .catch((error) => {
+        this.error = error;
+      });
+      
+  }
+
+  renderedCallback(){
+    if(this.connectedCallbackexecuted === true){
+      console.log("this.oppCurrIsoCodeValue renderedCallback INfo :: " + this.oppCurrIsoCodeValue);
+      console.log("this.currCodes renderedCallback INfo :: " + JSON.stringify(this.currCodes));
+      this.oppCurrCoversionRate = this.currCodes.find((obj) => { return obj.IsoCode === this.oppCurrIsoCodeValue;}).ConversionRate;
+    }
+    
+  }
 }
